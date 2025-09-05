6ff44662ce7fe6e6ea7c998c49bb4703120b02ea
Bwesun <48540955+Bwesun@users.noreply.github.com>
2025-09-05 07:38:32 +0100

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.agriconnect',
+  appName: 'agriconnect',
+  webDir: 'dist'
+};
+
+export default config;
