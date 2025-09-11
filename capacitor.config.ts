08503c6e668148add1962bb6e3df0c248e34417d
Bwesun <48540955+Bwesun@users.noreply.github.com>
2025-09-11 04:27:53 +0100

    Fix appName formatting in capacitor.config.ts for consistency

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'ionic.agriconnect',
-  appName: 'Horti Connect',
+  appName: 'HortiConnect',
   webDir: 'dist'
 };
 
