b96782ab95240b2d2d029d6e6d95f54b5d4e4819
Bwesun <48540955+Bwesun@users.noreply.github.com>
2025-09-11 04:26:13 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,7 +1,6 @@
 import { IonButton, IonIcon, IonImg, IonText } from "@ionic/react";
 import React from "react";
 import Logo from '../../public/favicon.png'
-import { notifications, person } from "ionicons/icons";
 import { ArrowRightLeft, BellIcon, ChevronsRight, LeafIcon, LifeBuoy } from "lucide-react";
 import { InAppBrowser, DefaultWebViewOptions, ToolbarPosition, iOSViewStyle, iOSAnimation } from '@capacitor/inappbrowser';
 
@@ -43,7 +42,7 @@ const TopNav: React.FC = () => {
             background: 'var(--ion-color-light)'
         }}>
             <div className=" flex items-center gap-4">
-                <LeafIcon size={28} className="text-amber-500" />
+                <LeafIcon size={28} className="text-amber-600" />
                 <IonText className="text-lg font-semibold" color="primary">HortiConnect</IonText>
             </div>
             <div className="flex justify-end items-center">
