d6e64c54c719333ccd018fe089a6a3f8feea3fa1
Bwesun <48540955+Bwesun@users.noreply.github.com>
2025-09-11 04:27:08 +0100

    Refactor code structure for improved readability and maintainability. Added android assets for icon splashscreen and icons foregroun and background

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'ionic.agriconnect',
-  appName: 'agriconnect',
+  appName: 'Horti Connect',
   webDir: 'dist'
 };
 
