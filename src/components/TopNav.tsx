f88663ba458f77b0ff04db59dfbe43b87a8bf130
Bwesun <48540955+Bwesun@users.noreply.github.com>
2025-09-24 15:15:47 +0100

    update webview URL in TopNav component

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -9,7 +9,7 @@ const TopNav: React.FC = () => {
     // To customize webview, u must specify all the attributes
     const openWebView = async () => {
         await InAppBrowser.openInWebView({
-            url: "https://ssp-ledger.vercel.app/",
+            url: "https://https://sspledger.com.ng/",
             options: {
                 showURL: false,
                 showToolbar: true,
